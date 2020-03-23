425f5fe93ec7e5ffa83c09dc40f82ad539853b8e
ilgiz <62037077+RIM666@users.noreply.github.com>
2020-03-23 16:43:58 +0300

    Update classlist.php

--- courses/first/classlist.php
+++ courses/first/classlist.php
@@ -1,15 +1,14 @@
 <?php
 
-//file: Classlist.php
+//file: classlist.php
 
 require_once "classes.php";
 
 function classlist($list)
 {
-    $className = '';
-   current($list);
-    $className .= current($list);
-    next ($list);
+    $className = ''; // Инициализация переменной
+    $className .= current($list); // Добавление первого элемента "b--user" к строке
+    next ($list); 
     while ($keyValue = current($list)) {
         if ($keyValue === true) {
             $className .= ' ' . key($list);
